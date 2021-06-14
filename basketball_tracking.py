f4175acefb06f66fed939e9633366f40bf0c566a
Tishya Chhabra <tatooine888@gmail.com>
2021-06-14 14:30:43 -0700

    played around with HSV value for basketball

--- basketball_tracking.py
+++ basketball_tracking.py
@@ -13,8 +13,8 @@ args = vars(ap.parse_args())
 
 # define the lower and upper boundaries of the "green" ball in the HSV color space
 # for a basketball, we'll need to change these values
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+brownLower = (10, 100, 20)
+brownUpper = (20, 255, 255)
 
 # initializing list of tracking points by using the argument passed earlier
 pts = deque(maxlen=args["buffer"])
@@ -52,7 +52,7 @@ while True:
 
 	# construct a mask for the color "green" -> basically filters out
     # anything that's not in the specified HSV range
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.inRange(hsv, brownLower, brownUpper)
 
     # series of erosions + dilations removes any remaining blobs
 	mask = cv2.erode(mask, None, iterations=2)
